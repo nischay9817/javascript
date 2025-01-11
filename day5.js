bfd73214e99cb27480e47eca9c7dccfc1d0b20d3
nischayshrestha <nikechrestha@gmail.com>
2025-01-11 22:10:15 +0545

    function part of js

---
+++ day5.js
@@ -0,0 +1,34 @@
+//function
+// function myFunction(){
+//     console.log("welcome to apna college");
+//     console.log("we are learning js:)");
+// }
+// myFunction();
+// function myFunction(msg){
+//     console.log(msg)
+// }
+// myFunction("I love JS");
+//multiple input
+// function myFunction(msg, num){
+//     console.log(msg, num);
+// }
+// myFunction("I love JS", 100);
+// function sum (x,y){
+//     console.log(x+y);
+// }
+// sum(1,2);
+// function sum(x,y){
+//     s=x+y;
+//     return s;
+// }
+// let val=sum(3,4);
+// console.log(val);
+//arrow function
+const arrowSum=(a,b)=>{
+    console.log(a+b);
+}
+arrowSum(5,4);
+const multiply= (x,y)=>{
+    console.log(x*y);
+}
+multiply(5,5);
